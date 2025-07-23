1b50457c48c8aa4fa3ac349d0c684e33f4660329
HAPPY <happy37135535@gmail.com>
2025-07-23 08:41:09 +0700

    Update Gunfight_Arena.lua

--- Gunfight_Arena.lua
+++ Gunfight_Arena.lua
@@ -98,7 +98,7 @@ RunService.RenderStepped:Connect(function()
 		if currentTarget and currentTarget:FindFirstChild("HumanoidRootPart") then
 			Camera.CFrame = CFrame.new(
 	        Camera.CFrame.Position,
-	        currentTarget.HumanoidRootPart.Position + Vector3.new(0, 2, 0))
+	        currentTarget.HumanoidRootPart.Position + Vector3.new(0, 1.75, 0))
 		end
 	elseif currentTarget then
 		removeESP(currentTarget)
